d16dacf578dacc43a958e1b5fc2ecbe4666f158c
Neil Sulhyan <neilsulhyan@gmail.com>
2025-12-22 23:40:29 +0800

    EmailJS Working now & new notification style

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,30 +8,53 @@ export default function Footer() {
     message: "",
   });
 
-  const [isSent, setIsSent] = useState(false);
+  const [notification, setNotification] = useState({
+    show: false,
+    type: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
+    setError("");
 
-    const serviceId = "service_7ykr66r";
+    const serviceId = "service_4tmz4tl";
     const templateId = "template_z21dawf";
     const userId = "fyqS7bBTPwx6vRZPG";
 
     emailjs
       .send(serviceId, templateId, formData, userId)
       .then((response) => {
-        console.log("SUCCESS!", response.status, response.text);
-        setIsSent(true);
+        setNotification({
+          show: true,
+          type: "success",
+          message: "Message sent successfully!",
+        })
+        setLoading(false);
         setFormData({
           name: "",
           email: "",
           message: "",
         });
+        setTimeout(() => {
+          setNotification({ show: false, type: "", message: "" });
+        }, 5000);
       })
       .catch((error) => {
         console.log("FAILED...", error);
         setError("Failed to send message. Please try again.");
+        setNotification({
+          show: true,
+          type: "error",
+          message: "Failed to send message. Please try again.",
+        });
+        setLoading(false);
+        setTimeout(() => {
+          setNotification({ show: false, type: "", message: "" });
+        }, 5000);
       });
   };
 
@@ -44,10 +67,27 @@ export default function Footer() {
   };
 
   return (
+    <>
+    {notification.show && (
+  <div
+    className={`fixed top-6 right-6 z-50 px-6 py-4 rounded shadow-lg text-white
+      ${
+        notification.type === "success"
+          ? "bg-green-500"
+          : "bg-red-500"
+      }
+      animate-slide-in
+    `}
+  >
+    {notification.message}
+  </div>
+)}
     <footer className="text-white py-8 px-4">
       <div className="max-w-7xl mx-auto flex flex-col justify-evenly md:flex-row">
         <div className="w-1/2">
-          <h2 className="text-2xl font-bold mb-4 text-blue-800 dark:text-[#22D3EE]">Contact Me</h2>
+          <h2 className="text-2xl font-bold mb-4 text-blue-800 dark:text-[#22D3EE]">
+            Contact Me
+          </h2>
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <input
@@ -55,7 +95,7 @@ export default function Footer() {
                 id="name"
                 name="name"
                 placeholder="Your Name"
-                className="w-full p-2 bg-gray-100 border border-gray-600 rounded"
+                className="w-full p-2 bg-gray-100 border border-gray-600 rounded dark:text-black"
                 value={formData.name}
                 onChange={handleChange}
                 required
@@ -67,7 +107,7 @@ export default function Footer() {
                 id="email"
                 name="email"
                 placeholder="Your Email"
-                className="w-full p-2 bg-gray-100 border border-gray-600 rounded"
+                className="w-full p-2 bg-gray-100 border border-gray-600 rounded dark:text-black"
                 value={formData.email}
                 onChange={handleChange}
                 required
@@ -78,7 +118,7 @@ export default function Footer() {
                 id="message"
                 name="message"
                 placeholder="Your Message"
-                className="w-full p-2 bg-gray-100 border border-gray-600 rounded"
+                className="w-full p-2 bg-gray-100 border border-gray-600 rounded dark:text-black"
                 value={formData.message}
                 onChange={handleChange}
                 required
@@ -86,15 +126,18 @@ export default function Footer() {
             </div>
             <button
               type="submit"
-              className="px-4 py-2 bg-blue-500 hover:bg-blue-600 dark:bg-[#22D3EE] dark:hover:bg-[#2296eeff] rounded"
+              disabled={loading}
+              className={`px-4 py-2 rounded transition-all
+              ${
+                loading
+                  ? "bg-gray-400 cursor-not-allowed"
+                  : "bg-blue-500 hover:bg-blue-600 dark:bg-[#22D3EE] dark:hover:bg-[#2296eeff]"
+              }
+            `}
             >
-              Submit
+              {loading ? "Sending..." : "Submit"}
             </button>
           </form>
-          {isSent && (
-            <p className="text-green-500 mt-4">Message sent successfully!</p>
-          )}
-          {error && <p className="text-red-500 mt-4">{error}</p>}
         </div>
         <div className="mt-8 md:mt-0">
           <div className="flex flex-col space-y-3">
@@ -128,19 +171,29 @@ export default function Footer() {
               Follow Me
             </h4>
             <div className="flex space-evenly space-x-4">
-              <a className="text-2xl text-black dark:text-white hover:text-red-600 dark:hover:text-red-600"
+              <a
+                className="text-2xl text-black dark:text-white hover:text-red-600 dark:hover:text-red-600"
                 href="https://in.pinterest.com/neilsulhyan/"
                 target="_blank"
               >
-                <i class="fa-brands fa-pinterest"></i>
+                <i className="fa-brands fa-pinterest"></i>
               </a>
-              <a className="text-2xl text-black dark:text-white hover:text-pink-600 dark:hover:text-pink-600" href="https://www.instagram.com/neilsulhyan10/" target="_blank">
+              <a
+                className="text-2xl text-black dark:text-white hover:text-pink-600 dark:hover:text-pink-600"
+                href="https://www.instagram.com/neilsulhyan10/"
+                target="_blank"
+              >
                 <i className="fa-brands fa-instagram"></i>
               </a>
-              <a className="text-2xl text-black dark:text-white dark:hover:text-gray-500 hover:text-gray-500" href="https://github.com/NeilSulhyan10" target="_blank">
+              <a
+                className="text-2xl text-black dark:text-white dark:hover:text-gray-500 hover:text-gray-500"
+                href="https://github.com/NeilSulhyan10"
+                target="_blank"
+              >
                 <i className="fa-brands fa-github"></i>
               </a>
-              <a className="text-2xl text-black dark:text-white hover:text-indigo-600 dark:hover:text-indigo-600"
+              <a
+                className="text-2xl text-black dark:text-white hover:text-indigo-600 dark:hover:text-indigo-600"
                 href="https://www.linkedin.com/in/neil-sulhyan-091ba82aa/"
                 target="_blank"
               >
@@ -151,8 +204,11 @@ export default function Footer() {
         </div>
       </div>
       <div className="mt-10 border-t border-gray-700 pt-4 flex items-center justify-center">
-        <p className="text-sm text-black dark:text-white">© 2025 Neil Sulhyan. All rights reserved.</p>
+        <p className="text-sm text-black dark:text-white">
+          © 2025 Neil Sulhyan. All rights reserved.
+        </p>
       </div>
     </footer>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,68 +1,73 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       keyframes: {
         meteor: {
-          '0%': {
-            transform: 'rotate(215deg) translateX(0)',
-            opacity: '1',
+          "0%": {
+            transform: "rotate(215deg) translateX(0)",
+            opacity: "1",
           },
-          '70%': {
-            opacity: '1',
+          "70%": {
+            opacity: "1",
           },
-          '100%': {
-            transform: 'rotate(215deg) translateX(-500px)',
-            opacity: '0',
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
           },
         },
         typing: {
-          '0%': {
-            width: '0%',
-            visibility: 'hidden',
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
           },
-          '100%': {
-            width: '100%',
+          "100%": {
+            width: "100%",
           },
         },
         blink: {
-          '50%': {
-            borderColor: 'transparent',
+          "50%": {
+            borderColor: "transparent",
           },
-          '100%': {
-            borderColor: 'white',
+          "100%": {
+            borderColor: "white",
           },
         },
         scroll: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' },
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
         },
         carousel: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-50%)' },
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+        slideIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(50px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
         },
       },
 
       animation: {
-        meteor: 'meteor 5s linear infinite',
-        typing: 'typing 4s steps(30) forwards, blink 0.7s infinite',
-        scroll: 'scroll 10s linear infinite',
-        carousel: 'carousel 30s linear infinite',
+        meteor: "meteor 5s linear infinite",
+        typing: "typing 4s steps(30) forwards, blink 0.7s infinite",
+        scroll: "scroll 10s linear infinite",
+        carousel: "carousel 30s linear infinite",
       },
 
       dropShadow: {
-        'cyan-sharp': '0 0 15px #22D3EE',
-        'yellow-sharp': '0 0 15px #FACC15',
-        'blue-sharp': '0 0 15px #1E40AF',
+        "cyan-sharp": "0 0 15px #22D3EE",
+        "yellow-sharp": "0 0 15px #FACC15",
+        "blue-sharp": "0 0 15px #1E40AF",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
